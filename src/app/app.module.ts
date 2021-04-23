f124c4f1dfc2eb59038ab57e55beb2f5856cb22e
Alonso <bermejoalonso7@gma.com>
2021-04-23 20:37:22 +0200

    Migracion Angular
    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HistoriaComponent} from './historia/historia.component';
+import {MainComponent} from './main/main.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  // {path: '', pathMatch: 'full', redirectTo: 'main'},
+  {path: '', component: MainComponent},
+  {path: 'history', component: HistoriaComponent}];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HistoriaComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/historia/temporada1/temporada1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-temporada1',
+  templateUrl: './temporada1.component.html',
+  styleUrls: ['./temporada1.component.scss']
+})
+export class Temporada1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
